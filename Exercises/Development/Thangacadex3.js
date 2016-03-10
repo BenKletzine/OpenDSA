66afae95903fac8a8c979c755b1dc2cf670326f2
Lor <lort70@uws26.acs.uwosh.edu>
2016-03-10 13:20:31 -0600

    Almost finished Khan Exercise, just needs CSS

--- Exercises/Development/Thangacadex3.js
+++ Exercises/Development/Thangacadex3.js
@@ -1,19 +1,22 @@
 /*global window */
 (function() {
+    
+   var style_default = {"background-color":"lightgray"};
+   var style_reject = {"background-color":"lightred"};
+   var style_accept = {"background-color":"lightgreen"};
+   var style_highlight = {"background-color":"yellow"};
+   var style_current = {"background-color":"lightteal"};
+    
+    
     "use strict";
     var my_array1 = [];
-    var my_array2 = []; //array one length
+    var my_array2 = []; //use array one length
     var av, jsavArr;
     var jsArr=[[],[]];
-    
-    //var rows = 2;
-    //var cols;
+    var array2length;
     
 	var counter = 0;
-	//var temp_index = 0;
     var myAnswer = 0;
-    
-    
     
     var thangacadex3 = {
 	
@@ -22,20 +25,22 @@
     initJSAV: function(arr_size)
     {
         jsArr[0]=my_array1;
-    jsArr[1]=my_array2;
+        jsArr[1]=my_array2;
         av = new JSAV("Thangacadex3", {animationMode: "none"});
         jsavArr = av.ds.matrix(jsArr);
         av.displayInit();
         av.recorded();
+        
+        //jsArr.css(0,0, style_highlight);
+        
+        
     },
 
 	// Initialize the exercise
-	initArr: function(arr_size, denom) { //Initialize ones array
+	initArr: function(arr_size, denom) { //Initialize Array One with ones
 	    var i;
 	    var next_val;
 	    var array_str;
-        //cols = arr_size;
-        //jsArr = initArr(rows,cols);
 
 	    my_array1 = [];
 	    for (i = 0; i < arr_size - 1; i++) {
@@ -56,21 +61,16 @@
 	    return array_str;
 	},
 
-	initArr2: function(loc, denom) {
+	initArr2: function(loc, denom) { //Initialize Array Two
 	    var i;
 	    var next_val = 0;
 	    var array_str;
-        counter = 0;
-        myAnswer = 0;
-		//var rand_num = Math.floor(Math.random() * 3 + 1);
+        counter = 0; //resets counter
+        myAnswer = 0; //resets answer
 	    my_array2 = [];
-        //my_array.push(0);
+        
 	    for (i = 0; i < loc - 1; i++) {
 
-           // if(i = 0)
-           // {
-           //     next_val = 0;
-           // }   
 		    if(i % denom == 0)
             {
                next_val = counter;
@@ -88,41 +88,46 @@
 	    for (i = 0; i < loc - 1; i++) {
 		    array_str = array_str + " " + my_array2[i];
 	    }
+        array2length = my_array2.length;
         
         myAnswer = next_val;
+        
+        while(my_array2.length != my_array1.length)
+        {
+            my_array2.push("");
+        }
         
 	    console.log(array_str);
 	    return array_str;
 	},
 	
 	nextValue: function(loc, denom) {
-	    //var i;
 	    var temp = 0;
 	    var temp_index = 0;
-        //var divide = 0;
         myAnswer++;
         
-        //if(my_array1[loc-1] > myAnswer && denom > myAnswer)
-        //{
-        //    my_array2[0] = my_array2[loc-1] + 1;
-        //}
-        
-        
-        if(loc == denom)
+        if(denom == 2)
         {
-            //divide = arr_size/ denom;
-            counter++;
-            myAnswer = counter;
+            if(my_array2[loc-1] == myAnswer || my_array2[loc-2] == myAnswer)
+                myAnswer++;
         }
-        else if(loc > denom) 
+        
+        if(denom == (loc-1))
         {
-            //counter++;
-            myAnswer = counter;
+            myAnswer = 1;
         }
-            my_array2[0] = myAnswer;
         
+        if((denom * 2) == (loc - 1))
+        {
+            myAnswer = 2;
+        }
         
-
+        if((denom * 3) == (loc - 1))
+        {
+            myAnswer = 3;
+        }
+        
+        my_array2[0] = myAnswer;
 		
 	    temp_index = Math.floor(Math.random() * (my_array2.length-1)) + 1;
 	    temp = my_array2[1];
@@ -139,6 +144,7 @@
 	    my_array2[3] = my_array2[temp_index];
 	    my_array2[temp_index] = temp;
         
+        //if second row has little value in array then it will make up value in options
         if(!my_array2[1])
         {
             my_array2[1] = 2;
@@ -154,13 +160,13 @@
             my_array2[3] = 4;
         }
 
-	    thangacadex3.option[0] = my_array2[1];
-	    thangacadex3.option[1] = my_array2[2];
-	    thangacadex3.option[2] = my_array2[3];
+	    thangacadex3.option[0] = my_array2[2];
+	    thangacadex3.option[1] = my_array2[3];
+	    thangacadex3.option[2] = my_array2[0] + 1;
 	    return my_array2[0];
 		
 	},
-
+    
     };
 
     window.thangacadex3 = window.thangacadex3 || thangacadex3;
